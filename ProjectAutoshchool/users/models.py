63a936c82d81231149018d0a38a78e0204d69658
Dmitriy <happy_river-horse@mail.ru>
2024-08-24 13:00:27 +0300

    update registration.html

--- ProjectAutoshchool/users/models.py
+++ ProjectAutoshchool/users/models.py
@@ -27,7 +27,7 @@ class ManagerProfile(models.Model):
     address = models.TextField(blank=True, null=True)
 
 
-class StudentProfile(models.Model):
+class StudentProfile(User):
     user = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'student'}, related_name='student_profile')
     phone = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
--- ProjectAutoshchool/users/views.py
+++ ProjectAutoshchool/users/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 
 
 def registration(request):
-    return render(request, 'registration.html', {})
+    if request.method == 'POST':
+        data = {
+            'login': request.POST.get('login'),
+            'password': request.POST.get('password'),
+            'repeat_password': request.POST.get('repeat_password'),
+            'ph_number': request.POST.get('ph_number'),
+            'date_birth': request.POST.get('date_birth'),
+        }
+
+        if data['password'] != data['repeat_password']:
+            return render(request, 'registration.html', {'error_password': 'Пароли не совпадают'})
+
+
+        return HttpResponse("СПАСИБО ВАШИ ДАННЫЕ УШЛИ НА СЕРВЕР")
+    elif request.method == 'GET':
+        print('ДАННЫЕ С СЕРВЕРА ОТПРАВИЛИСЬ')
+        return render(request, 'registration.html', {})
