8950c6fd5b38138bbe2b3da318c5e43c0c321ad2
Дима <you@example.com>
2024-11-03 16:15:13 +0300

    реализовал созданиеи просмотр вопросов

--- ProjectAutoshchool/tests/urls.py
+++ ProjectAutoshchool/tests/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import add_question
+from .views import add_question, view_questions, question_detail
 
 urlpatterns = [
     path('add_question/', add_question, name='add_question'),
+    path('view_questions/', view_questions, name='view_questions'),
+    path('question_detail/<int:question_id>/', question_detail, name='question_detail')
 ]
--- ProjectAutoshchool/tests/views.py
+++ ProjectAutoshchool/tests/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from .models import Question
+from lib2to3.fixes.fix_input import context
 
+from django.shortcuts import render, redirect
+
+from users.models import TeacherProfile
+from .models import Question, Answer
 
 # количество вариантов ответов
 NUMBER_OF_RESPONSES = 4
@@ -12,6 +15,7 @@ def add_question(request):
             'title': 'Создать вопрос',
             'number_of_responses': range(1, NUMBER_OF_RESPONSES + 1)
         }
+        return render(request, 'add_question.html', context=context)
     elif request.method == 'POST':
         question_data = {
             'question_text': request.POST.get('question_text'),
@@ -19,11 +23,44 @@ def add_question(request):
             'author': request.user.teacher_profile,
             'question_image': request.FILES.get('question_image')
         }
-        print(question_data)
-        # Question.objects.create(**question_data)
+        question = Question.objects.create(**question_data)
         for number in range(1, NUMBER_OF_RESPONSES + 1):
-            answer_data = {
+            answer_text = request.POST.get(f'answer{number}')
+            if answer_text:
+                answer_data = {
+                    'question': question,
+                    'answer_text': answer_text,
+                    'is_correct': True if request.POST.get(f'correct_answer{number}') else False
+                }
+                Answer.objects.create(**answer_data)
+        return redirect('general')
 
-            }
-        context = {}
-    return render(request, 'add_question.html', context=context)
+
+def view_questions(request):
+    teacher = request.GET.get('author')
+    if teacher:
+        if teacher == 'deleted_authors':
+            questions = Question.objects.filter(author_id=None).prefetch_related('answers')
+        else:
+            questions = Question.objects.filter(author_id=teacher).prefetch_related('answers')
+    else:
+        questions = Question.objects.prefetch_related('answers').all()
+    authors = TeacherProfile.objects.all()
+    context = {
+        'title': 'Просмотр вопросов',
+        'questions': questions,
+        'authors': authors
+    }
+    return render(request, 'view_questions.html', context=context)
+
+
+def question_detail(request, question_id):
+    question = Question.objects.prefetch_related('answers').get(id=question_id)
+    answer_count = question.answers.count()
+    if request.method == 'GET':
+        return render(request, 'question_detail.html', {
+            'title': 'Просмотр вопроса',
+            'author': question.author.user.get_full_name() if question.author else 'Автор удалён',
+            'question': question,
+            'answer_count': range(answer_count + 1, NUMBER_OF_RESPONSES + 1) if answer_count < NUMBER_OF_RESPONSES else []
+        })
